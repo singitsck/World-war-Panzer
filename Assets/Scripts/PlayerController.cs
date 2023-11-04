3107b72cc3af2f8d7590b32181c4c7ac8c06ee05
singitsck <chunkitsiu@rocketmail.com>
2023-11-05 03:26:39 +0800

    repull

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -54,6 +54,6 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveAmount);
+        //rb.MovePosition(rb.position + moveAmount);
     }
 }
